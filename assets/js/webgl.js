595f44d34df30d8bb2f21519b7c1c8752ce3e84a
Daniel <Daniel@DESKTOP-RBEMLC7>
2018-07-13 01:20:51 +0300

    mr. Kitty and social icons

--- assets/js/webgl.js
+++ assets/js/webgl.js
@@ -1,24 +1,28 @@
-/* $(window).scroll(function(){
-  var a = $('.wrap').scrollTop();
-  console.log(a);
-          if (a > 0) {
-           $('.navbar').css('min-height', 70);
-         } else $('.navbar').css('min-height', 50);
+ /*$(window).scroll(function(){
+var a = $('.wrap').scrollTop();
+console.log(a);
+        if (a > 0) {
+         $('.navigation').addClass('nav-height');
+         $('.nav-button-item').addClass('nav-underline-small').removeClass('nav-underline');
+       } else {
+         $('.navigation').removeClass('nav-height nav-underline-small');
+         $('.nav-button-item').addClass('nav-underline').removeClass('nav-underline-small'); }
 
        }); */
+
 $(document).ready(function() {
   $('.wrap').bind("scroll", function () {
 
     var a = $('.wrap').scrollTop();
     console.log(a);
             if (a > 0) {
-             $('.navbar').addClass('nav-height');
+             $('.navigation').addClass('nav-height');
              $('.nav-button-item').addClass('nav-underline-small').removeClass('nav-underline');
            } else {
-             $('.navbar').removeClass('nav-height nav-underline-small');
+             $('.navigation').removeClass('nav-height nav-underline-small');
              $('.nav-button-item').addClass('nav-underline').removeClass('nav-underline-small'); }
 
     }
   );
 
-})
+}) 
